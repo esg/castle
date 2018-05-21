f2ec7904a0a11064298bf8a9e50eab1ce19bd629
Emil Skoeldberg <emil@skoeldberg.net>
2018-05-21 18:08:28 +0100

    Let the main function generate a random line from the grammar.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,15 @@
--- A skeletal main function.
+module Main where
+
+import PGF
+import System.Random
+import Paths_castle
+
+
 
 main :: IO ()
-main = return ()
+main = do
+  pgf <- getDataFileName "gf/Castle.pgf"
+  gr <- readPGF pgf
+  let lang : _ = languages gr
+  g <- getStdGen
+  putStrLn $ linearize gr lang (head (generateRandom g gr (startCat gr)))
