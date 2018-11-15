cc5153cee3aac32275bd57abdfbaa6d4d43b6809
Emil Skoeldberg <emil@skoeldberg.net>
2018-11-15 14:59:13 +0000

    Improve the main program and incorporate probabilities.

--- Main.hs
+++ Main.hs
@@ -1,15 +1,50 @@
 module Main where
 
 import PGF
+import Control.Concurrent
+import Control.Monad
+import Data.Char
+import Data.Maybe
+import System.Environment
+import System.Exit
 import System.Random
 import Paths_castle
 
 
+normalize :: String -> String
+normalize =
+    let ss (' ' : '.' : cs) = '.' : (ss cs)
+        ss (c : cs) = c : (ss cs)
+        ss [] = []
+    in
+      (map toUpper) . ss
+
+loop :: [String] -> IO ()
+loop (s : ss) = do
+  putStrLn $ normalize s
+  threadDelay 2000000
+  loop ss
+
+parseArgs [] = usage >> exit
+parseArgs (arg : _) = return arg
+
+usage = putStrLn "Usage: castle LANG"
+exit = exitWith ExitSuccess
 
 main :: IO ()
-main = do
+main = getArgs  >>= parseArgs >>= generate
+
+generate :: String -> IO ()
+generate arg = do
   pgf <- getDataFileName "gf/Castle.pgf"
+  prf <- getDataFileName "gf/Castle.probs"
   gr <- readPGF pgf
-  let lang : _ = languages gr
-  g <- getStdGen
-  putStrLn $ linearize gr lang (head (generateRandom g gr (startCat gr)))
+  pr <- readProbabilitiesFromFile prf gr
+  let pgr =  setProbabilities pr gr
+  let lang = fromJust $ readLanguage ("Castle" ++ arg)
+  case lang `elem` languages gr of
+    True -> do
+        g <- getStdGen
+        loop $ map (linearize pgr lang) (generateRandom g pgr (startCat pgr))
+    False -> do
+        putStrLn $ "castle: Unknown language '" ++ arg ++ "'"
